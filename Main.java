4c8a695f4353f238ed1ef6f6cb854965d0d0ce5b
jonah141414 <jonah.fancylin@student.tdsb.on.ca>
2025-03-22 17:57:29 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+	public class Main{
+		public static void main(String[] args) {
+			System.out.println("Bingo Card \n \nThe rules of this game are simple, the caller will randomly pull a numbered bingo ball \n \nThe number will be called out on the bingo board and placed \n \nPlayers will then look for this number on their bingo card, in which they will mark it off when located \n \nThese steps are repeated until someone matches the bingo pattern, and when they do, they will yell out \"BINGO!\"\n");
+				System.out.format("%37s %8s %8s %8s %8s", "B", "I", "N", "G", "O\n");
+				System.out.format("%37s %8s %8s %8s %8s", "15", "17", "36", "58", "63\n");
+				System.out.format("%37s %8s %8s %8s %8s", "3", "19", "39", "51", "69\n");
+				System.out.format("%37s %8s %8s %8s %8s", "7", "24", "FREE", "47", "74\n");
+				System.out.format("%37s %8s %8s %8s %8s", "12", "27", "41", "48", "72\n");
+				System.out.format("%37s %8s %8s %8s %8s", "8", "30", "43", "55", "66\n");
+		    
+
+	
+}
+	}
